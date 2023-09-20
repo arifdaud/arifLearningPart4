5aa0ec21adf438face10cf865a655f58d68faff0
Arifyo <arif63446@gmail.com>
2023-09-20 11:36:28 +0800

    Added text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,9 @@
       <div class="card-header"><h1>Trade or View your Portfolio</h1></div>
       <div class="card-body">
         <h5 class="card-title">
-          <h5>You may Save & Load your Data</h5>
+          <h5>You may Save & Load your Data !!</h5>
           <h5>Clock on 'End Day' to begin a new Day !</h5>
+          <h5>TEST TEST</h5>
         </h5>
         <p class="card-text">Your Fund:{{ funds | currency }}</p>
       </div>
